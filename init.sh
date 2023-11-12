8bb3688083a466b77bcb38fe6ac3f5afe5e6a8e3
Lars Hudson Lunde <laffen47@yahoo.no>
2023-11-12 22:44:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,12 @@
 FILE=/install/firstrun
 if test -f "$FILE"; then
     rm -f /install/firstrun
+    mv /install/default /config/.
+    cp -f /config/default /etc/nginx/sites-enabled/.
     echo "Docker RuTorrent has been installed"
     echo "Start the docker the normal way and it will start normally"
     exit 0
 fi
+
 nginx
 read  -n 1 -p "Input Selection:" mainmenuinput
