78a74de32432f1b88563b1b5481c00eb02df74ff
Lars Hudson Lunde <laffen47@yahoo.no>
2023-11-13 20:53:30 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,29 +4,6 @@ FILE=/install/firstrun
 if test -f "$FILE"; then
     rm -f /install/firstrun
 
-    apt-get update
-	apt-get install -y dialog \
- 		screen \
- 		tmux \
-		nginx \
-		zip \
-		rtorrent \
-		php \
-		php-fpm \
-		php-cli  \
-		php-dev  \
-		php-xml  \
-		php-curl  \
-		php-xmlrpc  \
-		php-json  \
-		php-mbstring  \
-		php-opcache  \
-		php-zip \
-		mediainfo \
-    		sox \
-      		geoip-database \
-		unrar-free
-
     mkdir -p /config/session
     mkdir -p /config/watch
     
