5a5d028cc7e23635b46b7d889c29ee06cd6608c2
Lars Hudson Lunde <laffen47@yahoo.no>
2023-11-13 15:17:07 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,12 +3,21 @@
 FILE=/install/firstrun
 if test -f "$FILE"; then
     rm -f /install/firstrun
+    
     mv /install/default /config/.
-    cp -f /config/default /etc/nginx/sites-enabled/.
+
+    unzip /install/ruTorrent*.zip -d /var/www/html > /dev/null
+    mv /var/www/html/ruTorrent* /var/www/html/rutorrent
+
+    mv /install/rtorrent.rc /config/.
+    
     echo "Docker RuTorrent has been installed"
     echo "Start the docker the normal way and it will start normally"
     exit 0
 fi
+
+cp -f /config/default /etc/nginx/sites-enabled/.
+cp -f /config/rtorrent.rc ~/.rtorrent.rc
 
 nginx
 read  -n 1 -p "Input Selection:" mainmenuinput
